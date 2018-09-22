0453779470374d95f8651a4039b8238d00621b14
davideastmond <davebuck1981@gmail.com>
2018-09-22 09:55:43 -0400

    First commit

---
+++ blasterdisaster.py
@@ -0,0 +1,120 @@
+import numpy as np
+import random as rnd
+from tkinter import Tk
+import time as tmr
+
+class Direction:
+    """
+    This can serve as our action space reference
+    """
+    Up = -1
+    Down = 1
+    Nothing = 0
+
+
+class Marker:
+    AGENT = 1
+    BEAM = 2
+    EMPTY = 0
+    LASER_HIT = 3
+
+
+class Laser:
+    """
+    Represents a laser beam on the star field
+    """
+    def __init__(self, init_coord):
+        """
+        :param init_row: this represents the start row of where a laser will be generated
+        #
+        """
+        self.coord = init_coord # This keeps track of the row, col of the laser beam
+
+
+class Environment:
+    """
+    This creates the main game environment
+    """
+    def __init__(self, p_size, int_max_lasers=3):
+        """
+        :param p_size: A tuple indicating size of the star field, typically (10, 10)
+        :param int_max_lasers: how many lasers can appear on the field.
+        """
+        self.StarField = np.zeros(p_size, dtype=int)
+        self.laser_array = []  # Keeps track of all the lasers in the field. These must be laser objects
+        self.agent_position = ()  # Keeps track of agent (x, y)
+        self.max_lasers = int_max_lasers
+        self.started = False
+
+    def spawn_laser(self, init_row):
+        """
+        Spawn a laser at row
+        :param init_row: row where the laser will be
+        :return: void
+        """
+
+        # Add a laser to the array providing it doesn't exceed the max
+        if len(self.laser_array) < self.max_lasers:
+            # Spawn a laser and add to the array
+            mypos = self.get_random_laser_position()
+            
+            # Append only if the spot is empty
+            if self.StarField[(mypos, self.StarField[1] - 1)] == Marker.Empty:
+                new_laser = Laser(mypos)
+                self.laser_array.append(new_laser)
+
+    def get_random_laser_position(self):
+        """
+        Returns an integer indicating the row of the laser
+        :return: int
+        """
+        return rnd.randint(0, self.StarField.shape[0])
+
+    def spawn_agent(self):
+        """
+        randomly places agent along (row, 1) at the beginning of a game
+        :return: void
+        """
+
+    def animate(self):
+        """
+        This goes through the laser array and animates each laser step by step, and will also
+        eliminate any laser
+        :return:
+        """
+
+        # First we'll place the laser in the star field
+        if len(self.laser_array) > 0:
+            # the laser array has at least one or more lasers in it.
+            for idx, lsr in enumerate(self.laser_array):
+                # Place a laser
+                row, col = lsr.coord
+                if col > 0:
+                    self.StarField[row, col] = Marker.BEAM  # Place the beam in the matrix
+                elif col == 0:
+                    # delete the laser
+                    self.laser_array.remove(lsr)
+
+
+    def check_contact(self):
+        """
+        This checks if a laser makes contact with the agent, garnering a negative reward,
+        If it does, the contacting laser should be despawned
+        :return:
+        """
+
+    def start(self):
+        """
+        This starts the environment and the loop
+        """
+
+
+def testing():
+    r = Environment((10, 10), 3)
+    print(r.StarField)
+
+    while True:
+        print(r.get_random_laser_position())
+        tmr.sleep(1)
+
+testing()
